4cb55f4dbc35f77f200bbdaec159152acd48fe11
valerezzz <zenvalere@gmail.com>
2024-11-28 20:39:15 +0100

    add coord right

--- js/utils.js
+++ js/utils.js
@@ -30,33 +30,71 @@ export default class Utils {
     const leftEye = px(landmarks[33]);
     const rightEye = px(landmarks[263]);
     const chin = px(landmarks[152]);
-    const front = px(landmarks[10]);
 
-    // Vecteurs
+    // Vecteur utilisé pour le calcul du roll
     const eyeVector = {
       x: rightEye.x - leftEye.x,
       y: rightEye.y - leftEye.y,
       z: rightEye.z - leftEye.z,
     };
+
+    // Normaliser le vecteur eyeVector pour le calcul du roll
+    const eyeVectorLength = Math.sqrt(
+      eyeVector.x * eyeVector.x +
+        eyeVector.y * eyeVector.y +
+        eyeVector.z * eyeVector.z
+    );
+    const normalizedEyeVector = {
+      x: eyeVector.x / eyeVectorLength,
+      y: eyeVector.y / eyeVectorLength,
+      z: eyeVector.z / eyeVectorLength,
+    };
+
+    // Vecteur utilisé pour le calcul du pitch
     const noseToChinVector = {
       x: chin.x - nose.x,
       y: chin.y - nose.y,
       z: chin.z - nose.z,
     };
-    const noseToFrontVector = {
-      x: front.x - nose.x,
-      y: front.y - nose.y,
-      z: front.z - nose.z,
+
+    // Point central entre les yeux, utilisé pour le calcul du yaw
+    const eyeCenter = {
+      x: (leftEye.x + rightEye.x) / 2,
+      y: (leftEye.y + rightEye.y) / 2,
+      z: (leftEye.z + rightEye.z) / 2,
+    };
+
+    // Vecteur utilisé pour le calcul du yaw
+    const noseToEyeCenterVector = {
+      x: eyeCenter.x - nose.x,
+      y: eyeCenter.y - nose.y,
+      z: eyeCenter.z - nose.z,
     };
 
     // Angles
-    const pitch = Math.atan2(noseToChinVector.y, -noseToChinVector.z);
-    const yaw = Math.atan2(noseToFrontVector.x, -noseToFrontVector.z);
-    const roll = Math.atan2(eyeVector.y, eyeVector.x);
+    const pitch = Math.atan2(-noseToChinVector.y, noseToChinVector.z);
+    // Utiliser le vecteur normalisé pour le yaw
+    const yaw = Math.atan2(noseToEyeCenterVector.x, -noseToEyeCenterVector.z);
+    // Calculer le roll uniquement à partir du vecteur des yeux projeté sur le plan XY
+    const roll = Math.atan2(normalizedEyeVector.y, normalizedEyeVector.x);
 
-    const pitchdeg = Math.round((pitch * 180) / Math.PI);
-    const yawdeg = Math.round((yaw * 180) / Math.PI);
-    const rolldeg = Math.round((roll * 180) / Math.PI);
+    let pitchdeg = Math.round((pitch * 180) / Math.PI) + 60;
+    let yawdeg = Math.round((yaw * 180) / Math.PI) - 180;
+    let rolldeg = Math.round((roll * 180) / Math.PI);
+
+    // Normalisation de l'angle yaw entre -180° et +180°
+    if (pitchdeg < -180) pitchdeg += 360;
+    if (pitchdeg > 180) pitchdeg -= 360;
+
+    if (yawdeg < -180) yawdeg += 360;
+    if (yawdeg > 180) yawdeg -= 360;
+
+    if (rolldeg < -180) rolldeg += 360;
+    if (rolldeg > 180) rolldeg -= 360;
+
+    const eyeDistance = Math.sqrt(
+      Math.pow(rightEye.x - leftEye.x, 2) + Math.pow(rightEye.y - leftEye.y, 2)
+    );
 
     // Bounding box
     let minX = Infinity,
@@ -80,10 +118,9 @@ export default class Utils {
       pitch: pitchdeg,
       yaw: yawdeg,
       roll: rolldeg,
-      posX: boxCenterX,
-      posY: boxCenterY,
-      sizeX: boxWidth,
-      sizeY: boxHeight,
+      eyeDistance: Math.round(eyeDistance), // Distance en pixels entre les yeux
+      eyeLeft: Math.round(leftEye.x) + "," + Math.round(leftEye.y), // Position de l'œil gauche
+      eyeRight: Math.round(rightEye.x) + "," + Math.round(rightEye.y), // Position de l'œil droit
     };
   }
 
@@ -102,5 +139,17 @@ export default class Utils {
       ctx.fill();
       ctx.closePath();
     }
+
+    // Dessiner le trait entre les yeux
+    const leftEye = px(landmarks[33]);
+    const rightEye = px(landmarks[263]);
+
+    ctx.beginPath();
+    ctx.moveTo(leftEye.x, leftEye.y);
+    ctx.lineTo(rightEye.x, rightEye.y);
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.closePath();
   }
 }
